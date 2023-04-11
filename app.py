28c7d310350c9a31706d91d1ab2c3954868f6824
Jose <ynujose@gmail.com>
2023-04-10 21:34:08 -0400

    portfolio

--- app.py
+++ app.py
@@ -4,43 +4,43 @@ from flask_mail import Mail, Message
 
 
 
-# def create_app():
+def create_app():
 
-app = Flask(__name__)
+    app = Flask(__name__)
 
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'josepinadevbusiness@gmail.com'
-app.config['MAIL_PASSWORD'] = 'cbixcbycusltlbhh'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+    app.config['MAIL_SERVER']='smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = 'josepinadevbusiness@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'cbixcbycusltlbhh'
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
 
-mail = Mail(app)
+    mail = Mail(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
-@app.route('/mail', methods=[ 'GET', 'POST'])
-def send_email():
+    @app.route('/mail', methods=[ 'GET', 'POST'])
+    def send_email():
 
-    if request.method == 'POST':
-        name = request.form.get('name')
-        subject = request.form.get('subject')
-        email = request.form.get('email')
-        message = request.form.get('message')
+        if request.method == 'POST':
+            name = request.form.get('name')
+            subject = request.form.get('subject')
+            email = request.form.get('email')
+            message = request.form.get('message')
 
 
-        msg = Message(
-            subject = 'Asunto del Mensaje: ' + subject,
-            body=f'Nombre: {name}\nEmail: <{email}>\n\nMensaje: {message}',
-            sender = email,
-            recipients = ['josepinadevbusiness@gmail.com']
-        )
-        mail.send(msg)
-        return render_template('mail.html' )
-    
-    return redirect(url_for('index'))
+            msg = Message(
+                subject = 'Asunto del Mensaje: ' + subject,
+                body=f'Nombre: {name}\nEmail: <{email}>\n\nMensaje: {message}',
+                sender = email,
+                recipients = ['josepinadevbusiness@gmail.com']
+            )
+            mail.send(msg)
+            return render_template('mail.html' )
+        
+        return redirect(url_for('index'))
 
-# return app
+    return app
 
