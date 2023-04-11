79b88986d00e0700ad919484f83d1aed78e57109
Jose <ynujose@gmail.com>
2023-04-11 00:11:14 -0400

    text

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, g
 
 from flask_mail import Mail, Message
 
@@ -6,9 +6,12 @@ from flask_mail import Mail, Message
 
 def create_app():
 
+
     app = Flask(__name__)
 
-    app.config['MAIL_SERVER']='smtp.gmail.com'
+    app.secret_key = 'llave_secreta'
+
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
     app.config['MAIL_USERNAME'] = 'josepinadevbusiness@gmail.com'
     app.config['MAIL_PASSWORD'] = 'cbixcbycusltlbhh'
@@ -17,11 +20,13 @@ def create_app():
 
     mail = Mail(app)
 
+
     @app.route('/')
     def index():
         return render_template('index.html')
+    
 
-    @app.route('/mail', methods=[ 'GET', 'POST'])
+    @app.route('/mail', methods=['GET', 'POST'])
     def send_email():
 
         if request.method == 'POST':
@@ -30,17 +35,16 @@ def create_app():
             email = request.form.get('email')
             message = request.form.get('message')
 
-
             msg = Message(
-                subject = 'Asunto del Mensaje: ' + subject,
+                subject='Asunto del Mensaje: ' + subject,
                 body=f'Nombre: {name}\nEmail: <{email}>\n\nMensaje: {message}',
-                sender = email,
-                recipients = ['josepinadevbusiness@gmail.com']
+                sender=email,
+                recipients=['josepinadevbusiness@gmail.com']
             )
             mail.send(msg)
-            return render_template('mail.html' )
-        
+            return render_template('mail.html')
+
         return redirect(url_for('index'))
 
-    return app
 
+    return app
